bd8c1d916666230d2a69af0c558dc4e865f46861
nishanicloud2-netizen <nishanicloud2@gmail.com>
2026-03-17 15:57:12 +0530

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,34 @@
+print("nishan thanuja ")
+print("National School of Business Management Green University")
+
+a = 22
+b = 7
+print("Result of 22 divided by 7 is " + str(a/b))
+
+operation = "sum"
+total = 8
+print("The " + operation + " of two numbers is " + str(total))
+
+length = 10
+width = 5
+print("The area of the rectangle is " + str(length*width) + " square units")
+
+name = input("nishan: ")
+print("Welcome " + name + "!")
+print("The data type of the variable name is " + str(type(name)))
+
+birth_year = int(input("2008.11.27: "))
+print("Your birth year is: " + str(birth_year))
+print("The data type of the variable birth_year is " + str(type(birth_year)))
+
+birth_year = int(input("2008: "))
+age = 17 - birth_year
+print("Your age is: " + str(age))
+
+num1 = float(input("2: "))
+num2 = float(input("8: "))
+print("The sum of the two numbers is: " + str(num1 + num2))
+print("The difference of the two numbers is: " + str(num1 - num2))
+print("The product of the two numbers is: " + str(num1 * num2))
+print("The division of the two numbers is: " + str(num1 / num2))
+print("The power of the two numbers is: " + str(num1 ** num2))
